5313dc1cf2c273d6f3b430e16978176abfff2f62
Ieda Shinsuke <shinsuke.ieda@un-t.com>
2023-06-08 20:26:55 +0900

    feat: composition api 追加

--- vue-composition-api/src/main.ts
+++ vue-composition-api/src/main.ts
@@ -9,31 +9,39 @@ type Todo = {
   completed: boolean;
 };
 
-const vue = window.Vue;
+const { createApp, ref } = window.Vue;
 
-vue
-  .createApp({
-    data() {
-      return {
-        todoList: [] as TodoList,
-      };
-    },
-    methods: {
-      /**
-       * @description
-       * サンプルデータとして TODO リストを取得して JSON を返却する
-       */
-      fetchTodo: async (): Promise<TodoList> => {
-        const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-        return await res.json();
-      },
-    },
-    created() {
-      // options API の記法だと型推論がうまく効かない
-      // @ts-ignore
-      this.fetchTodo().then((res) => {
-        this.todoList = res;
+/**
+ * @description
+ * サンプルデータとして TODO リストを取得して JSON を返却する
+ */
+const fetchTodo = async (): Promise<TodoList> => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return await res.json();
+};
+
+createApp({
+  setup() {
+    const todoList = ref<TodoList>([]);
+
+    /**
+     * @description
+     * TODO リストを取得してデータに反映する
+     */
+    const setTodo = (data: TodoList) => {
+      todoList.value = data;
+    };
+
+    const init = () => {
+      fetchTodo().then((todoList) => {
+        setTodo(todoList);
       });
-    },
-  })
-  .mount("#app");
+    };
+
+    init();
+
+    return {
+      todoList,
+    };
+  },
+}).mount("#table");
